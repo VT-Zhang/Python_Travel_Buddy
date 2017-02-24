d7b20dc138cd6a76da74135144179dc0be34a324
VT-Zhang <jianz@vt.edu>
2017-02-24 15:59:15 -0500

    a

--- Travel_Buddy/settings.py
+++ Travel_Buddy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6!(kn!=$%m7yy6jb8ge*^+4-zv1_1qx607c$g^0!kmcvnn!i@='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.145.212.7']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
